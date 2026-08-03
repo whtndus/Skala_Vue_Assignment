b8d5734ea6730e8006d4bc7a4d5d6735be5671cf
조수연 <suyeun0109@naver.com>
2026-08-03 11:36:25 +0900

    Initial commit: Vue Weather Assignment

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import WeatherApp from './components/WeatherApp.vue'
+</script>
+
+<template>
+  <div class="app-container">
+    <WeatherApp />
+  </div>
+</template>
+
+<style scoped>
+.app-container {
+  padding: 1rem 0;
+}
+</style>
